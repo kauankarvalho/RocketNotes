f7164df5111fd1e38a7e269ad69e0f43547f1fbb
kauankarvalho <kauan.karvalho@outlook.com>
2023-09-28 19:26:53 -0300

    feat: add fonts, assets, and colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,19 @@
 export default {
   content: ["./src/pages/**/*.{js,jsx}"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      roboto_slab: ["Roboto Slab", "serif"],
+    },
+    colors: {
+      "gray-900": "#232129",
+      "gray-800": "#312E38",
+      "gray-700": "#666360",
+      "gray-600": "#3E3B47",
+      "gray-500": "#999591",
+      orange: "#FF9000",
+      white: "#F4EDE8",
+    },
     extend: {},
   },
   plugins: [],
