3ae2591d107b6c5625b42d4b8041e03b7bb9fa61
kauankarvalho <kauan.karvalho@outlook.com>
2023-10-06 10:19:43 -0300

    refactor: enhance Button component styling

--- web/src/components/Button.jsx
+++ web/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({ title }) {
   return (
-    <button className="bg-orange rounded-[1rem] p-[1.6rem] text-[1.6rem]/[2.1rem] font-medium text-gray-800 mt-[1.6rem]">
+    <button className="bg-orange rounded-[1rem] p-[1.6rem] text-[1.6rem]/[2.1rem] font-medium text-gray-800 w-full mt-[2.4rem]">
       {title}
     </button>
   )
