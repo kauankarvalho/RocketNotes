ba4da039071d67b46bf05b7f907606e648ab5f65
kauankarvalho <kauan.karvalho@outlook.com>
2023-10-09 16:00:01 -0300

    feactor: improve variable name for textClass

--- web/src/components/TextButton.jsx
+++ web/src/components/TextButton.jsx
@@ -1,10 +1,10 @@
 export function TextButton({ title, isOrange }) {
-  const textClass = isOrange ? "text-orange" : "text-gray-500"
+  const textColor = isOrange ? "text-orange" : "text-gray-500"
 
   return (
     <span
       type="button"
-      className={`text-[1.6rem]/[2.1rem] cursor-pointer ${textClass}`}
+      className={`text-[1.6rem]/[2.1rem] cursor-pointer ${textColor}`}
     >
       {title}
     </span>
