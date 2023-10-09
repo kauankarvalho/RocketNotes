38378c92495e1844efcdca8013d700f285a04f9e
kauankarvalho <kauan.karvalho@outlook.com>
2023-10-09 16:01:52 -0300

    feat: add flex box

--- web/src/components/Section.jsx
+++ web/src/components/Section.jsx
@@ -1,7 +1,7 @@
 export function Section({ title, children }) {
   return (
-    <div>
-      <h2 className="text-[2rem]/[2.6rem] text-gray-500 pb-[1.6rem] border-b-[0.1rem] border-gray-700">
+    <div className="flex gap-[2rem] flex-wrap justify-between">
+      <h2 className="text-[2rem]/[2.6rem] text-gray-500 pb-[1.6rem] border-b-[0.1rem] border-gray-700 mb-[0.5rem] w-full">
         {title}
       </h2>
       {children}
