16ced2b4dbbb0787465a5878b28daef51fb2a143
kauankarvalho <kauan.karvalho@outlook.com>
2023-10-09 16:03:36 -0300

    feat: add the ability to add new properties

--- web/src/components/Button.jsx
+++ web/src/components/Button.jsx
@@ -1,6 +1,9 @@
-export function Button({ title }) {
+export function Button({ title, ...rest }) {
   return (
-    <button className="bg-orange rounded-[1rem] p-[1.6rem] text-[1.6rem]/[2.1rem] font-medium text-gray-800 w-full mt-[2.4rem]">
+    <button
+      className="bg-orange rounded-[1rem] p-[1.6rem] text-[1.6rem]/[2.1rem] font-medium text-gray-800 w-full mt-[2.4rem]"
+      {...rest}
+    >
       {title}
     </button>
   )
