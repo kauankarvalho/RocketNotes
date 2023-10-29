dcce8760621f70f8f729231780e1df7677fe5c13
kauankarvalho <kauan.karvalho@outlook.com>
2023-10-28 23:11:15 -0300

    feat: enable users to add a profile photo

---
+++ server/src/controllers/userAvatarController.js
@@ -0,0 +1,38 @@
+const DiskStorage = require("../providers/diskStorage")
+const prisma = require("../database")
+
+class UserAvatarController {
+  async update(request, reponse) {
+    const avatar = request.file.filename
+    const { id } = request.user
+
+    const diskStorage = new DiskStorage()
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    const avatarExists = user.avatar
+
+    if (avatarExists) {
+      await diskStorage.deleteFile(user.avatar)
+    }
+
+    await diskStorage.saveFile(avatar)
+
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        avatar,
+      },
+    })
+
+    return reponse.status(200).json()
+  }
+}
+
+module.exports = UserAvatarController
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,12 +1,24 @@
+const UserAvatarController = require("../controllers/userAvatarController")
 const UserController = require("../controllers/userController")
 const ensureAuth = require("../middlewares/ensureAuth")
+const uploadConfig = require("../configs/upload")
 const { Router } = require("express")
+const multer = require("multer")
 
 const userController = new UserController()
+const userAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig.multerConfig)
 
 const userRouter = Router()
 
 userRouter.post("/", userController.create)
 userRouter.put("/", ensureAuth, userController.update)
+userRouter.patch(
+  "/avatar",
+  ensureAuth,
+  upload.single("avatar"),
+  userAvatarController.update,
+)
 
 module.exports = userRouter
