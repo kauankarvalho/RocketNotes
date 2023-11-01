a115c3ba6baf0bf70a6300d4d8d4f3be1a6ad7ec
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-01 15:49:44 -0300

    feat: redirect to the home page if the user is authenticated

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
 import { AuthRoutes } from "./auth.routes"
 import { AppRoutes } from "./app.routes"
+import { useAuth } from "../hooks/auth"
 
 export function Routes() {
-  return (
-    <BrowserRouter>
-      <AuthRoutes />
-    </BrowserRouter>
-  )
+  const { user } = useAuth()
+
+  const routesToRender = user ? <AppRoutes /> : <AuthRoutes />
+
+  return <BrowserRouter>{routesToRender}</BrowserRouter>
 }
