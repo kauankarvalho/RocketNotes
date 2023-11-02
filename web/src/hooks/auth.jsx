807334bb95a10baf5acb12a3f6af6571c9ccbff3
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-02 14:34:04 -0300

    feat: add authentication local storage

--- web/src/hooks/auth.jsx
+++ web/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 import { api } from "../services/api"
 
 const AuthContext = createContext()
@@ -16,12 +16,31 @@ export function AuthProvider({ children }) {
         const { user, token } = response.data
 
         api.defaults.headers.authorization = `Bearer ${token}`
+
+        localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+        localStorage.setItem("@rocketnotes:token", token)
+
         setData({ user, token })
       })
       .catch((error) => {
         alert(error.response.data.message)
       })
   }
+
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketnotes:user")
+    const token = localStorage.getItem("@rocketnotes:token")
+
+    const userAndTokenExists = user && token
+    if (userAndTokenExists) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        user: JSON.parse(user),
+        token,
+      })
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
