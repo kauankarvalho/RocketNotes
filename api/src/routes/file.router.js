6d6664c4c1c9ad15e51ea725803dc3c7512d78d8
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-04 11:21:03 -0300

    refactor: remove the authentication middleware from the /file route

--- api/src/routes/file.router.js
+++ api/src/routes/file.router.js
@@ -1,10 +1,9 @@
-const ensureAuth = require("../middlewares/ensureAuth")
 const uploadConfig = require("../configs/upload")
 const { Router } = require("express")
 const express = require("express")
 
 const fileRouter = Router()
 
-fileRouter.use("/", ensureAuth, express.static(uploadConfig.uploadsFolder))
+fileRouter.use("/", express.static(uploadConfig.uploadsFolder))
 
 module.exports = fileRouter
