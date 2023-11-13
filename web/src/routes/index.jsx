56fef41ec77699e91d6e8ac961f4550786780ef3
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-13 09:03:47 -0300

    refactor: enhance route names in the web application

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
-import { AuthRoutes } from "./auth.routes"
-import { AppRoutes } from "./app.routes"
+import { AuthRoutes } from "./authRoutes"
+import { AppRoutes } from "./appRoutes"
 import { useAuth } from "../hooks/auth"
 
 export function Routes() {
