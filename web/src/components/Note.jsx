40135aa6dd12f94442d6e44221fda9a9e74475d8
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-21 22:26:57 -0300

    feat: add animation to note card

--- web/src/components/Note.jsx
+++ web/src/components/Note.jsx
@@ -2,7 +2,7 @@ export function Note({ title, onClick, children }) {
   return (
     <div
       id="Note"
-      className="bg-gray-700 w-full py-[1.6rem] px-[2.2rem] rounded-[1rem] flex flex-col gap-[2.4rem] cursor-pointer"
+      className="bg-gray-700 w-full py-[1.6rem] px-[2.2rem] rounded-[1rem] flex flex-col gap-[2.4rem] cursor-pointer transition-all duration-[0.3s] hover:translate-y-[-0.5rem] hover:shadow-[-0.5rem_0.8rem_2rem_rgba(0,_0,_0,_0.2)]"
       onClick={onClick}
     >
       <h1 className="text-[2.4rem]/[3.2rem] text-white font-bold">{title}</h1>
