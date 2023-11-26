86b5f4b480e18d34eab957b6ddf8c9e786d10200
kauankarvalho <kauan.karvalho@outlook.com>
2023-11-26 11:53:27 -0300

    feat: enforce lowercase formatting for valid emails

--- api/src/utils/EmailValidator.js
+++ api/src/utils/EmailValidator.js
@@ -2,7 +2,7 @@ const ErrorResponse = require("./ErrorResponse")
 
 class EmailValidator {
   static validator(email) {
-    const emailValidator = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const emailValidator = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
 
     const invalidEmail = !emailValidator.test(email)
     if (invalidEmail) {
